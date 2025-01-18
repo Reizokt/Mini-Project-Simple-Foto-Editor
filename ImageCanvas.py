e564ae1e6bd76440efa755554af47ad512c7e8bd
chris001002 <chris001002@gmail.com>
2025-01-18 16:20:07 +0700

    Updated ui, some features

--- ImageCanvas.py
+++ ImageCanvas.py
@@ -1,42 +1,162 @@
 import tkinter as tk
 from tkinter import Canvas
 import cv2
-from tkinter import filedialog
 from PIL import Image, ImageTk
+import numpy as np
 class ImageCanvas(tk.Canvas):
     def __init__(self, master):
-        Canvas.__init__(self, master=master, bg="white", width=600, height=400)
+        self.width = 600
+        self.height = 400
+        Canvas.__init__(self, master=master, bg="white", width=self.width, height=self.height)
         self.shown_image = None
         self.ratio = 0
+        self.rotation_degree = 0
+        self.translate = (0,0)
+        self.resize = (0,0)
+        self.color_manipulation = (1,1,1)
+        self.isGrayScale = False
+        self.color_filters = []
+        self.brightness = 1
+        self.contrast = 1
+        self.border = None
+        self.padding = 0
+        self.contrast_stretch = False
 
     def show_image(self, img=None):
         self.delete("all")
-        if img is None: print("Error")
-        image = None
-
-        if len(img.shape) == 2:image =img
-        else: image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        
+        image = img
+        if img is None:
+            print("No image to show")
+            return
+        if self.isGrayScale:
+            image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        else:
+            image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if self.rotation_degree != 0:
+            image = Image.fromarray(image).rotate(self.rotation_degree)
+            image = np.array(image)
         height, width = image.shape[0], image.shape[1]
+        if self.translate != (0,0):
+            print(image)
+            T = np.array([[1,0,self.translate[0]],[0,1,self.translate[1]]],dtype=np.float32)
+            image = cv2.warpAffine(image, T, (width, height))
+        if self.resize != (0,0):
+            image = cv2.resize(image, (int(width*self.resize[0]), int(height*self.resize[1])))
+            height, width = image.shape[0], image.shape[1]
+        if self.color_manipulation != (1,1,1) and len(image.shape) == 3:
+            image = np.clip(image * self.color_manipulation, 0, 255).astype(np.uint8)
+        if self.contrast_stretch:
+            image = self.contrast_stretching(image)
+        if len(self.color_filters) > 0:
+            for filter in self.color_filters:
+                print("test2")
+                if filter == "sepia":
+                    # Sepia filter
+                    print("test3")
+                    kernel = np.array([[0.393, 0.769, 0.189],  # Red channel
+                                    [0.349, 0.686, 0.168],  # Green channel
+                                    [0.272, 0.534, 0.131]])  # Blue channel
+                    image = cv2.transform(image, kernel)  # Apply sepia tone
+                    image = cv2.convertScaleAbs(image, alpha=1, beta=35)  # Adjust brightness
+
+                elif filter == "cyanotype":
+                    # Cyanotype filter (emphasizing blue tones in RGB)
+                    blue_channel_boost = np.zeros_like(image)
+                    blue_channel_boost[..., 2] = 125  # Boost blue channel in RGB
+                    image = cv2.add(image, blue_channel_boost)
+
+                elif filter == "vignette":
+                    # Vignette effect
+                    rows, cols = image.shape[:2]
+                    kernel_x = cv2.getGaussianKernel(cols, cols / 4)
+                    kernel_y = cv2.getGaussianKernel(rows, rows / 4)
+                    kernel = kernel_y @ kernel_x.T
+                    mask = kernel / kernel.max()  # Normalize the mask
+                    for i in range(3):  # Apply to all RGB channels
+                        image[..., i] = image[..., i] * mask    
+        image = cv2.convertScaleAbs(
+                image,
+                alpha=self.brightness,
+                beta=50 * (self.contrast - 1)
+            )
+        if self.border != None:
+            if self.border == cv2.BORDER_CONSTANT:
+                image = cv2.copyMakeBorder(
+                    image,
+                    self.padding, self.padding, self.padding, self.padding,
+                    self.border,
+                    value=(0,0,0)
+                )
+            else:
+                image = cv2.copyMakeBorder(
+                    image,
+                    self.padding, self.padding, self.padding, self.padding,
+                    self.border
+                )
         ratio = height / width
         new_width = width
         new_height = height
 
-        if height>self.winfo_height() or width > self.winfo_width():
+        if height>self.height or width > self.width:
             #Orientation is landscape
             if ratio<1:
-                new_width = self.winfo_width()
+                new_width = self.width
                 new_height = int(new_width * ratio)
             #Portrait
             else:
-                new_height = self.winfo_height()
+                new_height = self.height
                 new_width = int(new_height*width/height)
             self.shown_image = cv2.resize(image, (new_width, new_height))
             self.shown_image = ImageTk.PhotoImage(Image.fromarray(self.shown_image))
             self.ratio = height/new_height
 
-            self.config(width=new_width, height=new_height)
+            self.config(width=new_width, height=new_height) 
             self.create_image(new_width/2, new_height/2, anchor= tk.CENTER, image = self.shown_image)
             
-
-
+    def save(self, path):
+        image_array = ImageTk.getimage(self.shown_image)
+        image_cv2 = np.array(image_array)
+        if self.isGrayScale:
+            image_cv2 = cv2.cvtColor(image_cv2, cv2.COLOR_RGB2GRAY)
+        else:
+            image_cv2 = cv2.cvtColor(image_cv2, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(path, image_cv2)
+    
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}("+
+            f"width={self.width}, "+
+            f"height={self.height}, "+
+            f"shown_image={self.shown_image}, "+
+            f"ratio={self.ratio}, "+
+            f"rotation_degree={self.rotation_degree}, "+
+            f"translate={self.translate}, "+
+            f"resize={self.resize}, "+
+            f"color_manipulation={self.color_manipulation}, "+
+            f"isGrayScale={self.isGrayScale}, "+
+            f"color_filters={self.color_filters}"+
+            ")"
+        )
+    def contrast_stretching(self, image):
+        def helper(image):
+            # Get the min and max pixel values
+            R_min = np.min(image)
+            R_max = np.max(image)
+            # Desired output range (0 to 255)
+            L_min = 0
+            L_max = 255
+            # Apply the contrast stretching formula
+            stretched = ((image - R_min) / (R_max - R_min)) * (L_max - L_min) + L_min
+            # Convert to uint8 type
+            stretched = np.uint8(stretched)
+            return stretched
+        if self.isGrayScale:
+            stretched = helper(image)
+            return stretched
+        else:
+            stretched = np.zeros_like(image)
+            for i in range(3):
+                stretched[:,:,i] = helper(image[:,:,i])
+            return stretched
+            
+            
--- main.py
+++ main.py
@@ -1,38 +1,64 @@
 import tkinter as tk
 import cv2
 from tkinter import filedialog
+from tkinter import ttk
 from PIL import Image, ImageTk
 from ImageCanvas import ImageCanvas
 import numpy as np
 image:np.ndarray = None
+border_types = {
+    "Border Replicate": cv2.BORDER_REPLICATE,
+    "Border Reflect": cv2.BORDER_REFLECT,
+    "Border Reflect 101": cv2.BORDER_REFLECT_101,
+    "Border Wrap": cv2.BORDER_WRAP,
+    "Border Constant": cv2.BORDER_CONSTANT,
+    "None":None
+}
 root = tk.Tk()
-root.geometry("1700x844")
+style = ttk.Style()
+style.theme_use("default")
+style.configure("TButton", 
+                foreground="white",
+                background="#003066",
+                font=("Arial", 12, "bold"),
+                padding = (10,10,10,10),
+                margin = (0,10,0,0)
+                )
+style.configure("TLabel",
+                foreground = "white",
+                background = "lightblue",
+                font = ("Arial", 12, "bold"),
+                margin = (0,10,0,0)
+                )
+style.configure("TScale",
+                background = "lightblue",
+                font = ("Arial", 12, "bold"),
+                margin = (0,10,0,0),
+                sliderthickness = 15,
+                sliderlength= 10,
+                troughrelief = "flat" 
+                )
+style.configure("TCheckbutton",
+                foreground = "white",
+                background = "lightblue",
+                font = ("Arial", 12),
+                margin = (0,10,0,0)
+                )
+
+sepia = tk.BooleanVar()
+cyanotype = tk.BooleanVar()
+vignette = tk.BooleanVar()
+root.state("zoomed")
 root.title("Three Pane Layout")
 
-# Configure grid layout
-root.grid_columnconfigure(0, weight=1)  # Left menu
-root.grid_columnconfigure(1, weight=3)  # Canvas
-root.grid_columnconfigure(2, weight=1)  # Right menu
-
-# Create frames
-frame1 = tk.Frame(root, bg="lightblue", width=200, height=844)  # Left menu
-frame2 = tk.Frame(root, bg="white", width=1000, height=844)     # Canvas (middle)
-frame3 = tk.Frame(root, bg="lightgreen", width=200, height=844) # Right menu
-
-# Add frames to the grid
-frame1.grid(row=0, column=0, sticky="nswe")
-frame2.grid(row=0, column=1, sticky="nswe")
-frame3.grid(row=0, column=2, sticky="nswe")
-
-# Add sample widgets
-tk.Label(frame1, text="Left Menu", bg="lightblue").pack(pady=10)
-tk.Label(frame2, text="Canvas Area", bg="white").pack(pady=10)
-tk.Label(frame3, text="Right Menu", bg="lightgreen").pack(pady=10)
-
-tk.Scale(frame1, from_=0, to=100, orient="horizontal").pack(pady=10)
-
-canvas1 = ImageCanvas(frame2)
-#Frame 1
+def color_manipulation():
+    global image
+    canvas1.color_manipulation = (slider_color_red.get()/100, slider_color_green.get()/100, slider_color_blue.get()/100)
+    canvas1.show_image(image)
+def convertGrayScale():
+    global image
+    canvas1.isGrayScale = not canvas1.isGrayScale
+    canvas1.show_image(image)
 def importImageClick():
     global image
     filename = filedialog.askopenfilename(
@@ -40,7 +66,6 @@ def importImageClick():
     filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif"),  # Include supported image formats
                ("All Files", "*.*")]  # Option to show all files
     )
-    image = cv2.imread(filename)
     if filename:  # Check if a file was selected
         image = cv2.imread(filename)
         if image is not None:  # Check if the image was successfully read
@@ -49,33 +74,343 @@ def importImageClick():
             print("Failed to load image. Please select a valid image file.")
     else:
         print("No file selected.")
-        
-
-label1 = tk.Label(frame2, text="Image 1", background="white")
-label1.pack()
-button = tk.Button(frame2, text="Import Image", command=importImageClick)
-button.pack()
-canvas1.pack()
-
-
-def convertGrayScale():
-    global image
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    canvas1.show_image(image)
-button = tk.Button(frame1, text="Gray scale",command=convertGrayScale)
-button.pack()
-
 def save():
-    global image
-    cv2.imwrite("Saved img.jpg",image)
-button_save = tk.Button(frame1, text="Save", command=save)
-button_save.pack()
-
+    path = filedialog.asksaveasfilename(
+    defaultextension=".png", 
+    filetypes=[("PNG files", "*.png"), ("JPG files", "*.jpg"), ("All files", "*.*")]
+)
+    if path:
+        canvas1.save(path)
 def notOperation():
     global image
     image = cv2.bitwise_not(image)
     canvas1.show_image(image)
 
-button_negative = tk.Button(frame1, text="Negative Transformation", command=notOperation)
-button_negative.pack()
+def onRotateChange(value):
+    global image
+    angle = int(value)
+    canvas1.rotation_degree = angle
+    canvas1.show_image(image)
+    print(repr(canvas1))
+
+def flipHorizontal():
+    global image
+    image = cv2.flip(image, 1)
+    canvas1.show_image(image)
+
+def flipVertical():
+    global image
+    image = cv2.flip(image, 0)
+    canvas1.show_image(image)
+
+def flipDiagonal():
+    global image
+    image = cv2.flip(image, 1)
+    image = cv2.flip(image, 0)
+    canvas1.show_image(image)
+
+def translate(e):
+    global image
+    try:
+        entry_x = entry_translatex.get() if entry_translatex.get() != "" else 0
+        entry_y = entry_translatey.get() if entry_translatey.get() != "" else 0
+        canvas1.translate = (int(entry_x), int(entry_y))
+        canvas1.show_image(image)
+    except ValueError:
+        print("Invalid input for translate")
+
+def scale(e):
+    global image
+    try:
+        entry_x = entry_scalingx.get() if entry_scalingx.get() != "" else 100
+        entry_y = entry_scalingy.get() if entry_scalingy.get() != "" else 100
+        entry_x = int(entry_x)
+        entry_y = int(entry_y)
+        canvas1.resize = (entry_x/100, entry_y/100)
+        canvas1.show_image(image)
+    except ValueError:
+        print("Invalid input for scale")
+
+def color_filter():
+    global image
+    color_filters = canvas1.color_filters
+    # Define the filters and their corresponding variables in a list
+    filter_options = [
+        ("sepia", sepia),
+        ("cyanotype", cyanotype),
+        ("vignette", vignette)
+    ]
+
+    # Iterate over the filters and update the color_filters list
+    for filter_name, filter_var in filter_options:
+        if filter_var.get() == 1 and filter_name not in color_filters:
+            color_filters.append(filter_name)
+        elif filter_var.get() == 0 and filter_name in color_filters:
+            color_filters.remove(filter_name)
+
+    # Update the canvas property
+    canvas1.color_filters = color_filters
+    canvas1.show_image(image)
+
+def brightnessChange(event):
+    global image
+    brightness = event.widget.get()
+    canvas1.brightness = float(brightness)
+    canvas1.show_image(image)
+
+def contrastChange(event):
+    global image
+    contrast = event.widget.get()
+    canvas1.contrast = float(contrast)
+    canvas1.show_image(image)
+def borderChange(event):
+    global image
+    border = event.widget.get()
+    canvas1.border = border_types[border]
+    canvas1.show_image(image)
+
+def paddingChange(event):
+    global image
+    padding = event.widget.get()
+    canvas1.padding = int(padding)
+    canvas1.show_image(image)
+def contrast_stretching():
+    canvas1.contrast_stretch = not canvas1.contrast_stretch
+    canvas1.show_image(image)
+def binaryOperation(operation):
+    global image
+    operations = {
+            "AND": cv2.bitwise_and,
+            "OR": cv2.bitwise_or,
+            "XOR": cv2.bitwise_xor,
+            "NOT": cv2.bitwise_not
+        }
+    if operation == "NOT":
+        if image is None: return
+        image = operations[operation](image)
+        canvas1.show_image(image)
+        return
+    filename = filedialog.askopenfilename(
+    title="Select the second Image File",
+    filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif"),  # Include supported image formats
+               ("All Files", "*.*")]  # Option to show all files
+    )
+    if filename:  # Check if a file was selected
+        image2 = cv2.imread(filename)
+        if image2 is not None:
+            image2 = cv2.resize(image2, (image.shape[1], image.shape[0]))
+            image = operations[operation](image, image2)
+            canvas1.show_image(image)
+        else:
+            print("Failed to load image. Please select a valid image file.")
+    else:
+        print("No file selected.")
+# Configure grid layout
+root.grid_columnconfigure(0, weight=1)  # Left menu
+root.grid_columnconfigure(1, weight=2)  # Canvas
+root.grid_columnconfigure(2, weight=1)  # Right menu
+root.grid_rowconfigure(0, weight=1)
+
+# Create frames
+frame1 = tk.Frame(root, background="lightblue", width=425, height=844)  # Left menu
+frame2 = tk.Frame(root, background="white", width=850, height=844)     # Canvas (middle)
+frame3 = tk.Frame(root, background="lightgreen", width=425, height=844) # Right menu
+
+# Add frames to the grid
+frame1.pack_propagate(False)
+frame1.grid(row=0, column=0, sticky="nswe")
+frame2.pack_propagate(False)
+frame2.grid(row=0, column=1, sticky="nswe")
+frame3.pack_propagate(False)
+frame3.grid(row=0, column=2, sticky="nswe")
+
+# Add sample widgets
+ttk.Label(frame1, text="Left Menu", font=("Arial", 20, "bold")).pack(pady=10)
+ttk.Label(frame2, text="Canvas Area", background="white").pack(pady=10)
+ttk.Label(frame3, text="Right Menu", background="lightgreen").pack(pady=10)
+
+canvas1 = ImageCanvas(frame2)
+        
+
+label1 = ttk.Label(frame2, text="Image 1", background="white")
+label1.pack()
+button = ttk.Button(frame2, text="Import Image", command=importImageClick)
+button.pack()
+canvas1.pack()
+button_save = ttk.Button(frame2, text="Save", command=save)
+button_save.pack()
+
+ttk.Label(frame1, text="Image Effects").pack()
+frame_image_effect = tk.Frame(frame1, background="lightblue")
+frame_image_effect.columnconfigure(0, weight=1, pad=10)
+frame_image_effect.columnconfigure(1, weight=1, pad=10)
+frame_image_effect.columnconfigure(2, weight=1, pad=10)
+
+button = ttk.Button(frame_image_effect, text="Gray scale",command=convertGrayScale)
+button.grid(column=0, row=0)
+button_negative = ttk.Button(frame_image_effect, text="Negative Transformation", command=notOperation)
+button_negative.grid(column=1, row=0)
+ttk.Button(frame_image_effect, text="Contrast stretching", command=contrast_stretching).grid(column=2, row=0)
+frame_image_effect.pack()
+
+
+label_rotate = ttk.Label(frame1, text="Rotate")
+label_rotate_degree = ttk.Label(frame1, text="0°")
+slider_rotate = ttk.Scale(frame1, from_=-180, to=180, orient="horizontal", command=lambda value: label_rotate_degree.config(text=f"Rotate: {float(value):.2f}°"))
+label_rotate.pack()
+label_rotate_degree.pack()
+slider_rotate.pack()
+slider_rotate.bind("<ButtonRelease-1>", lambda event: onRotateChange(slider_rotate.get()))
+
+
+button_flipv_icon = ImageTk.PhotoImage(Image.open("icons/vertical_flip.png").resize((30,30)))
+button_fliph_icon = ImageTk.PhotoImage(Image.open("icons/horizontal_flip.png").resize((30,30)))
+button_flipd_icon = ImageTk.PhotoImage(Image.open("icons/diagonal_flip.png").resize((30,30)))
+
+label_flip = ttk.Label(frame1, text="Flip", background="lightblue")
+label_flip.pack()
+container_flip = tk.Frame(frame1, background="lightblue")
+button_fliph = ttk.Button(container_flip, image=button_fliph_icon, command=flipHorizontal)
+button_fliph.grid(row=0, column=0)
+button_flipv = ttk.Button(container_flip, image=button_flipv_icon, command=flipVertical)
+button_flipv.grid(row=0, column=1)
+button_flipd = ttk.Button(container_flip, image=button_flipd_icon, command=flipDiagonal)
+button_flipd.grid(row=0, column=2)
+container_flip.pack()
+
+
+    
+
+label_translate = ttk.Label(frame1, text="Translate", background="lightblue")
+label_translate.pack()
+container_translate = tk.Frame(frame1, background="lightblue")
+label_translatex = ttk.Label(container_translate, text="X:")
+label_translatex.pack(side="left", padx=10)
+entry_translatex = tk.Entry(container_translate)
+entry_translatex.bind("<Return>", translate)
+entry_translatex.pack(side="left")
+label_translatex2 = ttk.Label(container_translate, text="px")
+label_translatex2.pack(side="left")
+label_translatey = ttk.Label(container_translate, text="Y:")
+label_translatey.pack(side="left", padx=10)
+entry_translatey = tk.Entry(container_translate)
+entry_translatey.bind("<Return>", translate)
+entry_translatey.pack(side="left")
+label_translatey2 = ttk.Label(container_translate, text="px")
+label_translatey2.pack(side="left")
+container_translate.pack()
+
+
+
+label_scaling = ttk.Label(frame1, text="Scale", background="lightblue")
+label_scaling.pack()
+container_scaling = tk.Frame(frame1, background="lightblue")
+label_scaling = ttk.Label(container_scaling, text="Scaling")
+label_scaling.pack()
+label_scalingx = ttk.Label(container_scaling, text="X:")
+label_scalingx.pack(side="left", padx=10)
+entry_scalingx = tk.Entry(container_scaling)
+entry_scalingx.pack(side="left")
+entry_scalingx.bind("<Return>", scale)
+label_scalingx2 = ttk.Label(container_scaling, text="%")
+label_scalingx2.pack(side="left")
+label_scalingy = ttk.Label(container_scaling, text="Y:")
+label_scalingy.pack(side="left", padx=10)
+entry_scalingy = tk.Entry(container_scaling)
+entry_scalingy.pack(side="left")
+entry_scalingy.bind("<Return>", scale)
+label_scalingy2 = ttk.Label(container_scaling, text="%")
+label_scalingy2.pack(side="left")
+container_scaling.pack()
+
+
+label_color_manipulation = ttk.Label(frame1, text="Color Manipulation", background="lightblue")
+label_color_manipulation.pack()
+
+frame_color_manipulation = tk.Frame(frame1, background="lightblue")
+frame_color_manipulation.grid_columnconfigure(0, weight=1, pad=10)
+frame_color_manipulation.grid_columnconfigure(1, weight=1, pad=10)
+frame_color_manipulation.grid_columnconfigure(2, weight=1, pad=10)
+
+
+label_color_red = ttk.Label(frame_color_manipulation, text="Red: 100")
+label_color_red.grid(row=0, column=0)
+slider_color_red = ttk.Scale(frame_color_manipulation, from_=0, to=200, orient="horizontal", command=lambda value: label_color_red.config(text=f"Red: {float(value):.0f}"))
+slider_color_red.grid(row=1, column=0)
+label_color_green = ttk.Label(frame_color_manipulation, text="Green: 100")
+label_color_green.grid(row=0, column=1)
+slider_color_green = ttk.Scale(frame_color_manipulation, from_=0, to=200, orient="horizontal", command=lambda value: label_color_green.config(text=f"Green: {float(value):.0f}"))
+slider_color_green.grid(row=1, column=1)
+label_color_blue = ttk.Label(frame_color_manipulation, text="Blue: 100")
+label_color_blue.grid(row=0, column=2)
+slider_color_blue = ttk.Scale(frame_color_manipulation, from_=0, to=200, orient="horizontal", command=lambda value: label_color_blue.config(text=f"Blue: {float(value):.0f}"))
+slider_color_blue.grid(row=1, column=2)
+frame_color_manipulation.pack()
+label_brightness = ttk.Label(frame1, text="Brightness: 1.0")
+brightness_slider = ttk.Scale(frame1, 
+                            from_=0.1, to=3.0, 
+                            orient=tk.HORIZONTAL, command=lambda value: label_brightness.config(text=f"Brightness: {float(value):.1f}"))
+brightness_slider.bind("<ButtonRelease-1>", brightnessChange)
+brightness_slider.set(1.0)
+label_brightness.pack()
+brightness_slider.pack()
+        
+# Contrast slider
+label_contrast = ttk.Label(frame1, text="Contrast:")
+contrast_slider = ttk.Scale(frame1, 
+                            from_=0.1, to=3.0, 
+                            orient=tk.HORIZONTAL, command=lambda value: label_contrast.config(text=f"Contrast: {float(value):.1f}"))
+contrast_slider.set(1.0)
+contrast_slider.bind("<ButtonRelease-1>", contrastChange)
+label_contrast.pack()
+contrast_slider.pack()
+label_color_filter = ttk.Label(frame1, text="Color Filter", background="lightblue")
+label_color_filter.pack()
+frame_filter = tk.Frame(frame1, background="lightblue")
+frame_filter.columnconfigure(0, weight=1, pad=10)
+frame_filter.columnconfigure(1, weight=1, pad=10)
+frame_filter.columnconfigure(2, weight=1, pad=10)
+checkbox_sepia = ttk.Checkbutton(frame_filter, text="Sepia", onvalue=1, offvalue=0, variable=sepia)
+checkbox_cyanotype = ttk.Checkbutton(frame_filter, text="Cyanotype", onvalue=1, offvalue=0, variable=cyanotype)
+checkbox_vignette = ttk.Checkbutton(frame_filter, text="Vignette", onvalue=1, offvalue=0, variable=vignette)
+for i, checkbox in enumerate([checkbox_sepia, checkbox_cyanotype, checkbox_vignette]):
+    checkbox.configure(command=lambda: color_filter())
+    checkbox.grid(row=0, column=i)
+frame_filter.pack()
+
+for i in [slider_color_red, slider_color_green, slider_color_blue]:
+    i.set(100)
+    i.bind("<ButtonRelease-1>", lambda e: color_manipulation())
+ttk.Label(frame1, text="Border Type:").pack()
+border_dropdown = ttk.Combobox(frame1,
+                            values=list(border_types.keys()),
+                            state="readonly")
+border_dropdown.set("None")
+border_dropdown.pack()
+border_dropdown.bind('<<ComboboxSelected>>', borderChange)
+
+# Padding slider
+ttk.Label(frame1, text="Padding:").pack()
+padding_slider = ttk.Scale(frame1, 
+                        from_=0, to=100, 
+                        orient=tk.HORIZONTAL)
+padding_slider.bind("<ButtonRelease-1>", paddingChange)
+padding_slider.pack()
+
+
+
+ttk.Label(frame3, text="Binary Operation", background="lightgreen").pack()
+
+frame_binary = ttk.Frame(frame3)
+frame_binary.columnconfigure(0, weight=1)
+frame_binary.columnconfigure(1, weight=1)
+frame_binary.columnconfigure(2, weight=1)
+frame_binary.columnconfigure(3, weight=1)
+
+ttk.Button(frame_binary, text="AND", command=lambda : binaryOperation("AND")).grid(column=0, row=0)
+ttk.Button(frame_binary, text="OR", command=lambda : binaryOperation("OR")).grid(column=1, row=0)
+ttk.Button(frame_binary, text="XOR", command=lambda : binaryOperation("XOR")).grid(column=2, row=0)
+ttk.Button(frame_binary, text="NOT", command=lambda : binaryOperation("NOT")).grid(column=3, row=0)
+frame_binary.pack()
+
 root.mainloop()
